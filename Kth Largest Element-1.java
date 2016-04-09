946774b12e80a4f7cc870aa47680a01036ecd2a5
winterfly <winterflyfei@gmail.com>
2016-04-09 19:51:34 -0400

    O(nlog(n)) time, O(1) space, 901ms

---
+++ Kth Largest Element-1.java
@@ -0,0 +1,11 @@
+class Solution {
+    /*
+     * @param k : description of k
+     * @param nums : array of nums
+     * @return: description of return
+     */
+    public int kthLargestElement(int k, int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
+};
