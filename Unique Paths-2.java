734ba29ea4e321d18dd36b4b43c3673976176e58
winterfly <winterflyfei@gmail.com>
2016-04-16 16:54:12 -0400

    O(mn) time, O(min(n,m)) space, 8793ms

---
+++ Unique Paths-2.java
@@ -0,0 +1,24 @@
+public class Solution {
+    /**
+     * @param n, m: positive integer (1 <= n ,m <= 100)
+     * @return an integer
+     */
+    public int uniquePaths(int m, int n) {
+        if (m < n) {
+            int temp = m;
+            m = n;
+            n = temp;
+        }
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+        }
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[j] += dp[j-1];
+            }
+        }
+        return dp[n-1];
+    }
+}
