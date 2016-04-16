e0a90617b405d95e193c3c4b7b1e11a865846157
winterfly <winterflyfei@gmail.com>
2016-04-16 18:37:48 -0400

    O(n^2) time, O(n) space, 1742ms

---
+++ Longest Increasing Subsequence.java
@@ -0,0 +1,27 @@
+public class Solution {
+    /**
+     * @param nums: The integer array
+     * @return: The length of LIS (longest increasing subsequence)
+     */
+    public int longestIncreasingSubsequence(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        for (int i = 1; i < n; i++) {
+            dp[i] = 1;
+            for (int j = i-1; j >= 0; j--) {
+                if (nums[j] <= nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+}
