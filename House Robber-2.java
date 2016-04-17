9c66f04b3523ebfae41cf9aaeb112576f4d0406e
winterfly <winterflyfei@gmail.com>
2016-04-17 17:03:23 -0400

    O(n) time, O(1) space, 3286ms

---
+++ House Robber-2.java
@@ -0,0 +1,18 @@
+public class Solution {
+    /**
+     * @param A: An array of non-negative integers.
+     * return: The maximum amount of money you can rob tonight
+     */
+    public long houseRobber(int[] A) {
+        if (A == null || A.length == 0) return 0;
+        if (A.length == 1)  return A[0];
+        int n = A.length;
+        long[] dp = new long[2];
+        dp[0] = A[0];
+        dp[1] = Math.max(dp[0], A[1]);
+        for (int i = 2; i < n; i++) {
+            dp[i%2] = Math.max(dp[(i-1)%2], dp[(i-2)%2] + A[i]);
+        }
+        return dp[(n-1)%2];
+    }
+}
